47c9a46fb547ec445a6dcbdc45c66e7550c72411
tnwjd2050 <tnwjd2050@naver.com>
2019-06-15 18:46:09 +0900

    live painting 그림판 생성/ 실시간 x,y 좌표 가져오기

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 import './login';
 import "./sockets";
 import "./chat";
+import "./paint";
 
 /* 
 ./sockets안에 import되어 있으므로 제거
--- src/socketController.js
+++ src/socketController.js
@@ -8,11 +8,22 @@ const socketController = socket => {
         socket.nickname = nickname;
         broadcast(events.newUser, { nickname });
     });
+    
     socket.on(events.disconnect, () => {
         broadcast(events.disconnected, { nickname : socket.nickname })
     });
+
     socket.on(events.sendMsg, ({message}) => {
         broadcast(events.newMsg, { message, nickname : socket.nickname })
+    });
+
+    socket.on(events.beginPath, ({ x, y }) =>
+        broadcast(events.beganPath, { x, y })
+    );
+
+    socket.on(events.strokePath, ({ x, y }) => {
+        broadcast(events.strokedPath, { x, y });
+        console.log(x, y);
     });
 };
 
